9aaa51bd7d197df6a21b01eff10007b7cfad424a
Samson <samsonadejoro@gmail.com>
2020-04-08 21:07:04 +0100

    Challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,62 @@
+def convertToDays(period_type, timeToElapse):
+  if period_type == 'days':
+    return timeToElapse
+
+  elif period_type == 'weeks':
+    return timeToElapse * 7
+
+  elif period_type == 'months':
+    return timeToElapse * 30
+
+  else:
+    return 'Incorrect input'
+
+
+
+
+
+period_type = input('Enter ')
+timeToElapse = int(input('Enter the number of days'))
+reportedCases = int(input('Enter the number of reported cases'))
+population = int(input('Population'))
+totalHospitalBeds = int(input('Hospital beds'))
+currentlyInfected = 0
+infectionsByRequestedTime = 0
+
+data = {
+
+    'data' : {
+      'period_type' : period_type,
+      'timeToElapse' : convertToDays(period_type, timeToElapse),
+      'reportedCases' : reportedCases,
+      'population' : population,
+      'totalHospitalBeds' : totalHospitalBeds
+    },
+
+    'impact' : {
+      'currentlyInfected' : reportedCases * 10,
+    },
+
+    'severeImpact' : {
+      'currentlyInfected' : reportedCases * 50,
+    }
+
+  }
+
+
+
+data['impact']['infectionsByRequestedTime'] = data['impact']['currentlyInfected'] * (2 * (timeToElapse/3))
+data['severeImpact']['infectionsByRequestedTime'] = data['severeImpact']['currentlyInfected'] * (2 * (timeToElapse/3))
+data['impact']['severeCasesByRequestedTime'] = data['impact']['infectionsByRequestedTime'] * (15/100)
+data['severeImpact']['severeCasesByRequestedTime'] = data['impact']['infectionsByRequestedTime'] * (15/100)
+
+
+
+print(data)
+
 def estimator(data):
+  
   return data
+
+
+print(estimator(data))
