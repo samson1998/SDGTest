daca438b5cb712074d81aa543c8677852d16b910
Samson <samsonadejoro@gmail.com>
2020-04-09 01:22:03 +0100

    Changes!

--- src/estimator.py
+++ src/estimator.py
@@ -47,14 +47,14 @@ def estimator(data):
 
   outputData['impact']['infectionsByRequestedTime'] = round(outputData['impact']['currentlyInfected'] * (2 ** (data['timeToElapse']/3)), 2)
   outputData['severeImpact']['infectionsByRequestedTime'] = round(outputData['severeImpact']['currentlyInfected'] * (2 ** (data['timeToElapse']/3)), 2)
-  outputData['impact']['severeCasesByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (15/100), 2)
-  outputData['severeImpact']['severeCasesByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (15/100), 2)
-  outputData['impact']['hospitalBedsByRequestedTime'] = outputData['data']['totalHospitalBeds'] - outputData['impact']['severeCasesByRequestedTime'] 
-  outputData['severeImpact']['hospitalBedsByRequestedTime'] = outputData['data']['totalHospitalBeds'] - outputData['severeImpact']['severeCasesByRequestedTime']
-  outputData['impact']['casesForICUByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (5/100), 2)
-  outputData['severeImpact']['casesForICUByRequestedTime'] = round(outputData['severeImpact']['infectionsByRequestedTime'] * (5/100), 2)
-  outputData['impact']['casesForVentilatorsByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (2/100), 2)
-  outputData['severeImpact']['casesForVentilatorsByRequestedTime'] = round(outputData['severeImpact']['infectionsByRequestedTime'] * (2/100), 2)
+  # outputData['impact']['severeCasesByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (15/100), 2)
+  # outputData['severeImpact']['severeCasesByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (15/100), 2)
+  # outputData['impact']['hospitalBedsByRequestedTime'] = outputData['data']['totalHospitalBeds'] - outputData['impact']['severeCasesByRequestedTime'] 
+  # outputData['severeImpact']['hospitalBedsByRequestedTime'] = outputData['data']['totalHospitalBeds'] - outputData['severeImpact']['severeCasesByRequestedTime']
+  # outputData['impact']['casesForICUByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (5/100), 2)
+  # outputData['severeImpact']['casesForICUByRequestedTime'] = round(outputData['severeImpact']['infectionsByRequestedTime'] * (5/100), 2)
+  # outputData['impact']['casesForVentilatorsByRequestedTime'] = round(outputData['impact']['infectionsByRequestedTime'] * (2/100), 2)
+  # outputData['severeImpact']['casesForVentilatorsByRequestedTime'] = round(outputData['severeImpact']['infectionsByRequestedTime'] * (2/100), 2)
 
 
   return outputData
