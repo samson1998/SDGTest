be13e68e2a9a1c19f02588a5ef87ea3578c3d941
Samson <samsonadejoro@gmail.com>
2020-04-09 01:11:38 +0100

    Changes!

--- src/estimator.py
+++ src/estimator.py
@@ -13,19 +13,19 @@ def convertToDays(period_type, timeToElapse):
 
 
 
-data = {
-'region': {
-    'name': "Africa",
-    'avgAge': 19.7,
-    'avgDailyIncomeInUSD': 5,
-    'avgDailyIncomePopulation': 0.71
-    },
-  'periodType': "days",
-  'timeToElapse': 58,
-  'reportedCases': 674,
-  'population': 66622705,
-  'totalHospitalBeds': 1380614
-}
+# data = {
+# 'region': {
+#     'name': "Africa",
+#     'avgAge': 19.7,
+#     'avgDailyIncomeInUSD': 5,
+#     'avgDailyIncomePopulation': 0.71
+#     },
+#   'periodType': "days",
+#   'timeToElapse': 58,
+#   'reportedCases': 674,
+#   'population': 66622705,
+#   'totalHospitalBeds': 1380614
+# }
 
 def estimator(data):
   
@@ -60,4 +60,4 @@ def estimator(data):
   return outputData
 
 
-print(estimator(data))
+estimator(data)
