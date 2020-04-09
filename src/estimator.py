435dff94ea52ea0ec709ed1297a8e7752cfb5ab4
Samson <samsonadejoro@gmail.com>
2020-04-09 11:29:31 +0100

    Challenge 1-3 completion

--- src/estimator.py
+++ src/estimator.py
@@ -13,19 +13,19 @@ def convertToDays(period_type, timeToElapse):
 
 
 
-data = {
-'region': {
-    'name': "Africa",
-    'avgAge': 19.7,
-    'avgDailyIncomeInUSD': 5,
-    'avgDailyIncomePopulation': 0.71
-    },
-  'periodType': "days",
-  'timeToElapse': 58,
-  'reportedCases': 674,
-  'population': 66622705,
-  'totalHospitalBeds': 1380614
-}
+# data = {
+# 'region': {
+#     'name': "Africa",
+#     'avgAge': 19.7,
+#     'avgDailyIncomeInUSD': 5,
+#     'avgDailyIncomePopulation': 0.71
+#     },
+#   'periodType': "days",
+#   'timeToElapse': 58,
+#   'reportedCases': 674,
+#   'population': 66622705,
+#   'totalHospitalBeds': 1380614
+# }
 
 def estimator(data):
   
@@ -62,4 +62,4 @@ def estimator(data):
   return outputData
 
 
-print(estimator(data))
+# print(estimator(data))
