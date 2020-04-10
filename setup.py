861c8597a2d8a0b2e27719e20439d58dc48e2970
Samson <samsonadejoro@gmail.com>
2020-04-10 23:15:58 +0100

    Final stage

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup, find_packages
 
 
-setup(name="covid19-estimator-py", packages=find_packages())
+setup(name="covid19-estimator-py", packages=find_packages(), install_requires=['django','rest_framework', 'rest_framework_tracking','python-simplexml'])
