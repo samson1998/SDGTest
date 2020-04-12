a5b8085da68775f26715e2dc2ff2d42d1d4c3fa9
Samson <samsonadejoro@gmail.com>
2020-04-12 14:20:38 +0100

    Little changes!

--- src/estimator.py
+++ src/estimator.py
@@ -64,20 +64,3 @@ def estimator(data):
   }
 
   return result
-
-# #test
-# tt = {
-#     "region": {
-#         "name": "Africa",
-#         "avgAge": 19.7,
-#         "avgDailyIncomeInUSD": 4,
-#         "avgDailyIncomePopulation": 0.73
-#     },
-#     "periodType": "days",
-#     "timeToElapse": 38,
-#     "reportedCases": 2747,
-#     "population": 92931687,
-#     "totalHospitalBeds": 678874
-# }
-
-# print(estimator(tt))
